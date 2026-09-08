30b7823b544818fb24d79feb22646c60aa8834b3
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-09-08 12:06:05 +0300

    test: match only Playwright e2e files

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './e2e',
+  testMatch: '**/*.e2e.js',
   timeout: 30_000,
   expect: {
     timeout: 10_000,
